ccfbed5c1f1d9c508d1460b0aaf28ad740f94286
Brian Faust <faustbrian@users.noreply.github.com>
2016-11-23 14:18:21 +0200

    Applied fixes from StyleCI (#8)
    [ci skip] [skip ci]

--- tests/ReviewTest.php
+++ tests/ReviewTest.php
@@ -1,9 +1,17 @@
 <?php
 
+/*
+ * This file is part of Laravel Reviewable.
+ *
+ * (c) Brian Faust <hello@brianfaust.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace BrianFaust\Tests\Reviewable;
 
 use BrianFaust\Reviewable\Review;
-use BrianFaust\Reviewable\Interfaces\HasReviews;
 
 class ReviewTest extends AbstractTestCase
 {
